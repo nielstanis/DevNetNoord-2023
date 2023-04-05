99042ec55f3211441d0a5639302e6c011519d237
Niels Tanis <nielstanis@live.nl>
2023-04-05 21:33:51 +0200

    Initial Content

---
+++ Middle/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using iLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello DevNetNoord 2023!");
+            Console.WriteLine($"ConsoleApp - {typeof(System.Net.Http.HttpClient).Assembly.Location}");
+            
+            var loadContext = new IsolatedLoadContext("../pub/lib.dll", new[]{ typeof(IProcessor) });
+            var d = loadContext.CreateInstance<IProcessor>();
+
+            //var result = await d.ProcessDocumentAsync("Docs/schedule.pdf","Output/schedule2022.pdf");
+            var result = await d.ProcessDocumentAsync("https://devnetnoord.nl/","Output/devnetnoord.html");
+            
+            Console.WriteLine("Done...");
+        }
+    }
+}
